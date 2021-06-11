e18c8b28f438d6b31702cb8c4ebc38efa52adf6d
saad <sa@gm.com>
2021-06-11 09:36:39 +0500

    signup done per not working

--- app/src/main/java/com/adnan/tech/im3ch/ProfilePicActivity.java
+++ app/src/main/java/com/adnan/tech/im3ch/ProfilePicActivity.java
@@ -1,18 +1,57 @@
 package com.adnan.tech.im3ch;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.adnan.tech.im3ch.Util.Anim;
+import com.github.dhaval2404.imagepicker.ImagePicker;
 
 public class ProfilePicActivity extends AppCompatActivity {
-
+    ImageView img_profile;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_pic);
         new Anim().Entry(this);
+        context=this;
+        img_profile=findViewById(R.id.img_profile);
+        img_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* ImagePicker.with(context)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();*/
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            //Image Uri will not be null for RESULT_OK
+            Uri uri  = data.getData();
+
+                    // Use Uri object instead of File to avoid storage permissions
+            img_profile.setImageURI(uri);
+        } else if (resultCode == ImagePicker.RESULT_ERROR) {
+            Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
