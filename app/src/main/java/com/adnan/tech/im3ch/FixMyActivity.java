c5a16a531441236fb7827542e2614db20de45f71
saad <sa@gm.com>
2021-07-17 13:52:19 +0500

    Image upload

--- app/src/main/java/com/adnan/tech/im3ch/FixMyActivity.java
+++ app/src/main/java/com/adnan/tech/im3ch/FixMyActivity.java
@@ -2,14 +2,21 @@ package com.adnan.tech.im3ch;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.adnan.tech.im3ch.Model.ModelAddress;
@@ -31,24 +38,35 @@ import com.squareup.okhttp.ResponseBody;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class FixMyActivity extends AppCompatActivity {
-    EditText et_location,et_make,et_model,et_year,et_budget,et_description;
+    EditText et_location, et_make, et_model, et_year, et_budget, et_description;
     MyPrefs prefs;
     ImageView img_location, img_item;
     String address, lat_long;
     Button btn_submit;
+    String fileuri;
     static int PICK_FROM_GALLERY = 1;
     Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fix_my);
-        context=this;
+        context = this;
         prefs = new MyPrefs(this);
         new Anim().Entry(this);
         et_location = findViewById(R.id.et_location);
@@ -63,50 +81,17 @@ public class FixMyActivity extends AppCompatActivity {
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String location,make,model,year,budget,description;
-                location=et_location.getText().toString();
-                make=et_make.getText().toString();
-                model=et_model.getText().toString();
-                year=et_year.getText().toString();
-                budget=et_budget.getText().toString();
-                description=et_description.getText().toString();
-                if(!(location.isEmpty()&&make.isEmpty()&&model.isEmpty()&&year.isEmpty()&&budget.isEmpty()&&description.isEmpty())) {
+                String location, make, model, year, budget, description;
+                location = et_location.getText().toString();
+                make = et_make.getText().toString();
+                model = et_model.getText().toString();
+                year = et_year.getText().toString();
+                budget = et_budget.getText().toString();
+                description = et_description.getText().toString();
+                uploadFile(fileuri);
+                if (!(location.isEmpty() && make.isEmpty() && model.isEmpty() && year.isEmpty() && budget.isEmpty() && description.isEmpty())) {
+                    uploadFile(fileuri);
                     OkHttpClient client = new OkHttpClient();
-                    MediaType mediaTypee = MediaType.parse("text/plain");
-                    RequestBody bodye = new RequestBody() {
-                        @Override
-                        public MediaType contentType() {
-                            return null;
-                        }
-
-                        @Override
-                        public void writeTo(BufferedSink sink) throws IOException {
-
-                        }
-                    }; /*new  MultipartBody.Builder().setType(MultipartBody.FORM)
-                            .addFormDataPart("file","/C:/Users/root/Pictures/Capture.PNG",
-                                    RequestBody.create(MediaType.parse("application/octet-stream"),
-                                            new File("/C:/Users/root/Pictures/Capture.PNG")))
-                            .build();*/
-                    Request requeste = new Request.Builder()
-                            .url("http://localhost:8081/upload")
-                            .method("POST", bodye)
-                            .build();
-                    client.newCall(requeste).enqueue(
-                            new Callback() {
-
-                                @Override
-                                public void onFailure(Request request, IOException e) {
-
-                                }
-
-                                @Override
-                                public void onResponse(Response response) throws IOException {
-
-                                }
-                            });
-
-
                     MediaType mediaType = MediaType.parse("application/json");
                     JSONObject jsonObject = new JSONObject();
                     try {
@@ -127,7 +112,6 @@ public class FixMyActivity extends AppCompatActivity {
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-                    MediaType mediaType = MediaType.parse("application/json");
                     RequestBody body = RequestBody.create(mediaType, jsonObject.toString());
                     Request request = new Request.Builder()
                             .url(new Api().URL + "car")
@@ -172,14 +156,14 @@ public class FixMyActivity extends AppCompatActivity {
                                     }
                                 }
                             });
+                }else{
+                    Toast.makeText(context,"Enter Values",Toast.LENGTH_SHORT).show();
                 }
             }
         });
         img_item.setOnClickListener(v -> {
             try {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, PICK_FROM_GALLERY);
+                mGetContent.launch("image/*");
             } catch (Exception ex) {
 
             }
@@ -207,18 +191,169 @@ public class FixMyActivity extends AppCompatActivity {
             }
 
         }
-        if (resultCode == RESULT_OK && requestCode == PICK_FROM_GALLERY) {
+        /*if (resultCode == RESULT_OK && requestCode == PICK_FROM_GALLERY) {
             try {
-                img_item.setImageURI(data.getData());
+                fileuri = data.getData();
+
+                img_item.setImageURI(fileuri);
             } catch (Exception ex) {
                 Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
             }
-        }
+        }*/
     }
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    try {
+                        // Handle the returned Uri
+                        String picturePath = convertMediaUriToPath(uri, context);
+                        Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                        fileuri = picturePath;
+                        img_item.setImageBitmap(thumbnail);
+                    }catch (Exception ex){
+                        Toast.makeText(getApplicationContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
 
+    String convertMediaUriToPath(Uri selectedImage, Context context ) {
+        String[] filePath = { MediaStore.Images.Media.DATA };
+        String[] bits = selectedImage.toString().split("/");
+        String filename = new Date().getTime() + bits[bits.length - 1];
+        String picturePath="";
+        if (filename.contains("%3A")) {
+            String id = filename.split("%3A")[1];
+            // val type=filename.split("%3a", ignoreCase = true).get(0)
+            Uri t = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            String sel = MediaStore.Images.Media._ID + "=?";
+            Cursor cursor = getContentResolver().query(
+                    t,
+                    filePath, sel, new String[]{id}, null
+            );
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            if (cursor.moveToFirst())
+                picturePath = cursor.getString(column_index);
+            cursor.close();
+        } else {
+
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(MediaStore.Images.Media.DATA);
+             picturePath = c.getString(columnIndex);
+            c.close();
+            Log.w("path of image gallery", picturePath + "");
+        }
+        return picturePath;
+    }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         new Anim().Back(this);
+    }
+
+    public int uploadFile(String sourceFileUri) {
+        int serverResponseCode=0;
+        String[] bits = sourceFileUri.split("/");
+        String fileName = new Date().getTime() + bits[bits.length - 1];
+
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+
+        if (!sourceFile.isFile()) {
+
+            //dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+                    + sourceFileUri + "" + fileName);
+
+
+            return 0;
+
+        } else {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(new Api().URL + "upload");
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("file", fileName);
+
+                dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=" + fileName + ";filename=" + fileName);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                 serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if (serverResponseCode == 200) {
+                    Toast.makeText(FixMyActivity.this, "File Upload Complete.",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+                //dialog.dismiss();
+                ex.printStackTrace();
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("Upload file to server", "Exception : "
+                        + e.getMessage(), e);
+            }
+
+            return serverResponseCode;
+
+        } // End else block
     }
 }
